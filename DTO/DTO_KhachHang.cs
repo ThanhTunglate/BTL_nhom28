8a8e519a3eaed570f846e626dbc6901877480273
tung thanh <thanhtung6920@gmail.com>
2021-09-03 20:32:27 +0700

    Add emtyti KhachHang

--- DTO/DTO_KhachHang.cs
+++ DTO/DTO_KhachHang.cs
@@ -8,6 +8,12 @@ namespace DTO
 {
     class DTO_KhachHang
     {
-        
+        string MaKH, HoTe, DiaChi, Sdt, MaNKH;
+
+        public string MaKH1 { get => MaKH; set => MaKH = value; }
+        public string HoTe1 { get => HoTe; set => HoTe = value; }
+        public string DiaChi1 { get => DiaChi; set => DiaChi = value; }
+        public string Sdt1 { get => Sdt; set => Sdt = value; }
+        public string Sdt1 { get => Sdt; set => Sdt = value; }
     }
 }
